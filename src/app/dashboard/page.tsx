511fa970f7511c588b9cfe7bed65c7dda47dd94b
MUmairqureshi <67610498+MUmairqureshi@users.noreply.github.com>
2023-10-27 03:46:00 -0700

    add

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -61,9 +61,9 @@ import { User } from '@/app/components/lib/schema'
                         </tr>
                     </thead>
                     <tbody>
-                        {res.data.map((data, id) => (<tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        {res.data.map((data) => (<tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
 
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <th key={data.id} scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {data.id}
                             </th>
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
